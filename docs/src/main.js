447d28f48405fe1721f139cedef79e37a9b3078e
Mansi S <mansisaini26@gmail.com>
2023-11-08 22:06:27 -0800

    more server packages

---
+++ docs/src/main.js
@@ -0,0 +1,17 @@
+// title of the game to displayed on the title screen
+title = "Scrabble Speed";
+
+// description, which is also displayed on the title screen
+description = 
+`Spacebar to jump`;
+
+// The array of custom sprites
+characters = []
+
+// game design variable container
+const G = {
+    WIDTH: 150,
+    HEIGHT: 100
+};
+
+// Game runtime options
